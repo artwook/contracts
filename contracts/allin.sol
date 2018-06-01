4e7d6449cd0bb19b27bfa0538ab275320eef5c8f
MarkChow <mark@MarkChowdeMacBook-Pro.local>
2018-06-01 17:16:27 +0800

    crowdsale add comment

--- contracts/allin.sol
+++ contracts/allin.sol
@@ -611,6 +611,11 @@ contract AKC is DSToken("AKC"), ERC223, Controlled {
     event ClaimedTokens(address indexed _token, address indexed _controller, uint _amount);
 }
 
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error.
+ */
 library SafeMath {
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     if (a == 0) {
@@ -638,20 +643,36 @@ library SafeMath {
   }
 }
 
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
 contract Ownable {
   address public owner;
 
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-  function Ownable() public {
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
     owner = msg.sender;
   }
 
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
   }
 
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
   function transferOwnership(address newOwner) public onlyOwner {
     require(newOwner != address(0));
     owner = newOwner;
@@ -660,27 +681,55 @@ contract Ownable {
 
 }
 
+/**
+ * @title Pausable
+ * @dev This let a contract have a paused status to pause and restart what contract want.
+ *
+ */
 contract Pausable is Ownable {
     bool public paused = false;
 
     event Pause();
     event Unpause();
 
+    /**
+     * @dev Throws if paused is true.
+     */
     modifier whenNotPaused() { require(!paused); _; }
+
+    /**
+     * @dev Throws if paused is false.
+     */
     modifier whenPaused() { require(paused); _; }
 
+    /**
+     * @dev Set paused to true.
+     */
     function pause() onlyOwner whenNotPaused public {
         paused = true;
         emit Pause();
     }
 
+    /**
+     * @dev Set paused to false.
+     */
     function unpause() onlyOwner whenPaused public {
         paused = false;
         emit Unpause();
     }
 }
 
+/**
+ * @title Withdrawable
+ * @dev Allow contract owner to withdrow Ether or ERC20 token from contract.
+ *
+ */
 contract Withdrawable is Ownable {
+    /**
+    * @dev withdraw Ether from contract
+    * @param _to The address to transfer to.
+    * @param _value The amount to be transferred.
+    */
     function withdrawEther(address _to, uint _value) onlyOwner public returns(bool) {
         require(_to != address(0));
         require(address(this).balance >= _value);
@@ -690,6 +739,12 @@ contract Withdrawable is Ownable {
         return true;
     }
 
+    /**
+    * @dev withdraw ERC20 token from contract
+    * @param _token ERC20 token contract adress.
+    * @param _to The address to transfer to.
+    * @param _value The amount to be transferred.
+    */
     function withdrawTokens(ERC20 _token, address _to, uint _value) onlyOwner public returns(bool) {
         require(_to != address(0));
 
@@ -697,6 +752,10 @@ contract Withdrawable is Ownable {
     }
 }
 
+/**
+ * @title ArtwookCoinCrowdsale
+ * @dev AKC token sale contract.
+ */
 contract ArtwookCoinCrowdsale is Pausable, Withdrawable {
   using SafeMath for uint;
 
@@ -707,9 +766,6 @@ contract ArtwookCoinCrowdsale is Pausable, Withdrawable {
       uint tokensSold;
       uint collectedWei;
 
-      bool transferBalance;
-      bool sale;
-      bool issue;
   }
   AKC public token;
   address public beneficiary = msg.sender;
@@ -724,32 +780,40 @@ contract ArtwookCoinCrowdsale is Pausable, Withdrawable {
   mapping(address => uint256) public canSell;
 
   event Purchase(address indexed holder, uint256 tokenAmount, uint256 etherAmount);
-  event Issue(address indexed holder, uint256 tokenAmount);
-  event Sell(address indexed holder, uint256 tokenAmount, uint256 etherAmount);
-  event NewRate(uint256 rate);
   event NextStep(uint8 step);
   event CrowdsaleClose();
 
+  /**
+  * @dev Initialize the crowdsale conditions.
+  * @param akctoken AKC token contract adress.
+  */
   function Crowdsale(AKC akctoken) public {
       require(token==address(0));
+      /* token = new AKC(); */
       token = akctoken;
       // crowdsale only sale 50% of totalSupply
       totalTokensForSale = 100000000 * 10 ** 18;
       uint num1 = 1 ether;
-      steps.push(Step(num1.div(5000000*100).mul(90), 0.01 ether, 1524105000, 0, 0, true, false, true));
-      steps.push(Step(num1.div(5000000*100).mul(95), 0.01 ether, 1524105120, 0, 0, true, false, true));
-      steps.push(Step(num1.div(5000000*100).mul(100), 0.01 ether, 1524105300, 0, 0, true, false, true));
+      steps.push(Step(num1.div(5000000*100).mul(90), 0.01 ether, 1524105000, 0, 0));
+      steps.push(Step(num1.div(5000000*100).mul(95), 0.01 ether, 1524105120, 0, 0));
+      steps.push(Step(num1.div(5000000*100).mul(100), 0.01 ether, 1524105300, 0, 0));
   }
 
-  // Fallback function to purchase tokens
+  /**
+  * @dev Fallback function that will delegate the request to purchase().
+  */
   function() external payable  {
       purchase(msg.sender);
   }
 
+  /**
+  * @dev purchase AKC
+  * @param sender The address to receive AKC.
+  */
   function purchase(address sender) whenNotPaused payable public {
       require(!crowdsaleClosed);
       require(now>steps[0].timestamp);
-
+      /*Update the step based on the current time.*/
       if (now > steps[1].timestamp && currentStep < 1){
         currentStep = 1;
         emit NextStep(currentStep);
@@ -761,32 +825,31 @@ contract ArtwookCoinCrowdsale is Pausable, Withdrawable {
       Step memory step = steps[currentStep];
 
       require(msg.value >= step.minInvestEth);
-
       require(totalTokensSold < totalTokensForSale);
 
       uint sum = msg.value;
       uint amount = sum.mul(1 ether).div(step.priceTokenWei);
       uint retSum = 0;
 
+      /* Calculate excess Ether */
       if(totalTokensSold.add(amount) > totalTokensForSale) {
           uint retAmount = totalTokensSold.add(amount).sub(totalTokensForSale);
           retSum = retAmount.mul(step.priceTokenWei).div(1 ether);
-
           amount = amount.sub(retAmount);
           sum = sum.sub(retSum);
       }
 
+      /* Record purchase info */
       totalTokensSold = totalTokensSold.add(amount);
       totalCollectedWei = totalCollectedWei.add(sum);
       steps[currentStep].tokensSold = step.tokensSold.add(amount);
       steps[currentStep].collectedWei = step.collectedWei.add(sum);
+      canSell[sender] = canSell[sender].add(amount);
 
-      if(currentStep == 0) {
-          canSell[sender] = canSell[sender].add(amount);
-      }
-
+      /* Mint and Send AKC */
       token.mint(sender, amount);
 
+      /* Return the excess Ether */
       if(retSum > 0) {
           sender.transfer(retSum);
       }
@@ -794,37 +857,16 @@ contract ArtwookCoinCrowdsale is Pausable, Withdrawable {
       emit Purchase(sender, amount, sum);
   }
 
-  function issue(address _to, uint256 _value) onlyOwner whenNotPaused public {
-      require(!crowdsaleClosed);
-
-      Step memory step = steps[currentStep];
-
-      require(step.issue);
-      /* require(step.tokensSold.add(_value) <= step.tokensForSale); */
-
-      steps[currentStep].tokensSold = step.tokensSold.add(_value);
-
-      if(currentStep == 0) {
-          canSell[_to] = canSell[_to].add(_value);
-      }
-
-      token.mint(_to, _value);
-
-      emit Issue(_to, _value);
-  }
-
+  /**
+  * @dev close crowdsale.
+  */
   function closeCrowdsale() onlyOwner public {
       require(!crowdsaleClosed);
-
+      /* Transfer the Ether from the contract to the beneficiary's adress.*/
       beneficiary.transfer(address(this).balance);
-      /* token.mint(beneficiary, token.cap().sub(token.totalSupply())); */
-      /* token.finishMinting(); */
-      /* token.transferOwnership(beneficiary); */
-      /* token.changeController(beneficiary); */
+      /* Set AKC contract owner to beneficiary.*/
       token.setOwner(beneficiary);
-
       crowdsaleClosed = true;
-
       emit CrowdsaleClose();
   }
 }
